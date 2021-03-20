cf1b0bceb673db61b9295e9c437151134fd75e72
RLogik <r.bitlogik@gmail.com>
2021-03-20 12:23:18 +0100

    dev > master: more reliable usage of ls

--- whales_setup/docker-entry.sh
+++ whales_setup/docker-entry.sh
@@ -12,9 +12,13 @@ echo "1" >| whales_setup/DOCKER_DEPTH;
 ## add prefix to logging levels:
 echo "export LOGGINGPREFIX=\">\";" >> whales_setup/.lib.logging.sh;
 
-## make all bash files in project root readable in unix and grant them execution permissions:
-ls -a {,whales_setup/{,.}}*.sh | xargs -i bash -c "dos2unix {}";
-ls -a {,whales_setup/{,.}}*.sh | xargs -i bash -c "chmod +x {}";
+## make all bash files in project root + whales_setup readable in unix and grant them execution permissions:
+ls -a {whales_setup/{,.}}*.sh | xargs -i bash -c "dos2unix {}";
+ls -a {whales_setup/{,.}}*.sh | xargs -i bash -c "chmod +x {}";
+ls -a *.sh | xargs -i bash -c "dos2unix {}"  2> /dev/null;
+ls -a *.sh | xargs -i bash -c "chmod +x {}"  2> /dev/null;
+ls -a .*.sh | xargs -i bash -c "dos2unix {}" 2> /dev/null;
+ls -a .*.sh | xargs -i bash -c "chmod +x {}" 2> /dev/null;
 
 ################################################################
 # !!! start of your code !!!
@@ -31,8 +35,8 @@ ls -a {,whales_setup/{,.}}*.sh | xargs -i bash -c "chmod +x {}";
 ################################################################
 
 ## add a few entries to bash-history:
-echo "cd /usr/apps/whales" >> $HOME/.bash_history;
-echo "./build.sh --mode setup" >> $HOME/.bash_history;
-echo "./test.sh --mode setup" >> $HOME/.bash_history;
+echo "cd /usr/apps/whales"     >> "$HOME/.bash_history";
+echo "./build.sh --mode setup" >> "$HOME/.bash_history";
+echo "./test.sh  --mode setup" >> "$HOME/.bash_history";
 
 echo "ENTRY SCRIPT FINISHED.";
