1d8d7bfb8119fb8d70d280168248b8cbea5be313
RLogik <r.bitlogik@gmail.com>
2021-06-04 13:31:31 +0200

    dev-go > master: go utils

--- golang/src/core/environment/environment.go
+++ golang/src/core/environment/environment.go
@@ -40,8 +40,11 @@ func ReadEnvKeyDefault(key string, valueDefault string, optional ...string) stri
  * PRIVATE
  * ---------------------------------------------------------------- */
 
-func loadEnvFile(fName string) error {
-	return godotenv.Load(fName)
+func loadEnvFile(path string) error {
+	if path == "" {
+		log.Fatal("Path name to environment file is not allowed to be empty!")
+	}
+	return godotenv.Load(path)
 }
 
 func getEnvKey(key string) string {
@@ -49,20 +52,20 @@ func getEnvKey(key string) string {
 }
 
 func readEnvKey(key string, optional ...string) (string, error) {
-	var fName string = ENV_FILE_NAME
+	var path string = ENV_FILE_NAME
 	var err error
 	var value string
 	if len(optional) > 0 {
-		fName = optional[0]
+		path = optional[0]
 	}
-	err = loadEnvFile(fName)
+	err = loadEnvFile(path)
 	if err != nil {
-		log.Fatalf("Could not read environment file, \033[1m%s\033[0m!\n", fName)
+		log.Fatalf("Could not read environment file, \033[1m%s\033[0m!\n", path)
 	}
 	err = nil
 	value = getEnvKey(key)
 	if value == "" {
-		err = errors.New(fmt.Sprintf("Environment key \033[1m%s\033[0m missing or empty in environment file \033[1m%s\033[0m!", key, fName))
+		err = errors.New(fmt.Sprintf("Environment key \033[1m%s\033[0m missing or empty in environment file \033[1m%s\033[0m!", key, path))
 	}
 	return value, err
 }
